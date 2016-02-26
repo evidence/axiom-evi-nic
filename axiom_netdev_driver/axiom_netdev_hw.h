bf7ec79c4407c8baac70a85b2d358c340054907d
Stefano Garzarella <s.garzarella@evidence.eu.com>
2016-02-26 15:23:23 +0100

    Change RAW TX registers

--- axiom_netdev_driver/axiom_netdev_hw.h
+++ axiom_netdev_driver/axiom_netdev_hw.h
@@ -16,32 +16,23 @@
 /* 32 bit r/w */
 #define AXIOMNET_IO_NODEID              16
 /* 32 bit w/o */
-#define AXIOMNET_IO_TXD_BASE            20
+#define AXIOMNET_IO_RAW_TX_BASE         20
 /* 32 bit w/o */
-#define AXIOMNET_IO_TX_START            24
+#define AXIOMNET_IO_RAW_TX_LEN          24
 /* 32 bit w/o */
-#define AXIOMNET_IO_RXD_BASE            28
+#define AXIOMNET_IO_RAW_TX_HEAD         28
 /* 32 bit w/o */
-#define AXIOMNET_IO_RX_START            32
-/* 32 bit w/o */
-#define AXIOMNET_IO_BUFSIZE             36
+#define AXIOMNET_IO_RAW_TX_TAIL         32
 
-#define AXIOMNET_IO_SIZE                40
+#define AXIOMNET_IO_SIZE                36
 
 /* Interrupt identifier */
 #define AXIOMNET_IRQ_GENERIC            0x00000001
 #define AXIOMNET_IRQ_RX                 0x00000002
 #define AXIOMNET_IRQ_ALL                0xFFFFFFFF
 
-struct axiom_netdev_bufdesc {
-    uint64_t buf_addr;
-    uint32_t length;
-    uint32_t flags;
-};
-
-/* Buffer flags */
-#define AXIOMNET_BUFDESC_EMPTY          0x00000001
-#define AXIOMNET_BUFDESC_LAST           0x00000002
+/* Default parameters */
+#define AXIOMNET_DEF_RING_LEN           256
 
 
 #endif /* AXIOM_NETDEV_REGS_H */
