7ad3475a2de2504b8e0f8c41d6758a9a6e8d6358
Stefano Garzarella <s.garzarella@evidence.eu.com>
2016-03-15 14:55:11 +0100

    axiom_switch: add include guards

--- axiom_switch/axiom_switch_logic.h
+++ axiom_switch/axiom_switch_logic.h
@@ -1,2 +1,8 @@
+#ifndef AXIOM_SWITCH_LOGIC_h
+#define AXIOM_SWITCH_LOGIC_h
+
 /* This function manages the axiom messages*/
 void receive_axiom_msg(char *buffer, int rc);
+
+
+#endif /* AXIOM_SWITCH_LOGIC_h */
