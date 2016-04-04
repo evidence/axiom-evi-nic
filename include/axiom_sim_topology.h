9b84591e2541df104994c74e54258e11ca672e49
Lisa <l.tani@evidence.com>
2016-04-04 13:01:31 +0200

    added num nodes parameter

--- include/axiom_sim_topology.h
+++ include/axiom_sim_topology.h
@@ -10,17 +10,12 @@
 #ifndef AXIOM_TOPOLOGY_h
 #define AXIOM_TOPOLOGY_H
 
-#define EXAMPLE0
-//#define EXAMPLE1
+#define EXAMPLE1
 //#define EXAMPLE2
 //#define EXAMPLE3
 //#define EXAMPLE4
 //#define EXAMPLE5
 
-
-#ifdef EXAMPLE0
-#define AXIOM_NUM_NODES               4
-#endif
 #ifdef EXAMPLE1
 #define AXIOM_NUM_NODES               8
 #endif
@@ -42,8 +37,8 @@
 #define AXIOM_NULL_NODE              255
 
 typedef struct axiom_topology {
-    uint8_t topology[AXIOM_NUM_NODES][AXIOM_NUM_INTERFACES];
-    uint8_t if_topology[AXIOM_NUM_NODES][AXIOM_NUM_INTERFACES];
+    uint8_t topology[AXIOM_MAX_NUM_NODES][AXIOM_NUM_INTERFACES];
+    uint8_t if_topology[AXIOM_MAX_NUM_NODES][AXIOM_NUM_INTERFACES];
     int num_nodes;
     int num_interfaces;
 } axiom_topology_t;
@@ -51,12 +46,12 @@ typedef struct axiom_topology {
 //extern axiom_topology_t start_topology;
 
 inline static void
-axsw_if_topology_init(axiom_topology_t *topology)
+axsw_if_topology_init(axiom_topology_t *topology, int num_nodes)
 {
     int node_index, if_index, i;
     uint8_t pair_node;
 
-    for (node_index = 0; node_index < AXIOM_NUM_NODES; node_index++)
+    for (node_index = 0; node_index < num_nodes; node_index++)
     {
         for (if_index = 0; if_index < AXIOM_NUM_INTERFACES; if_index++)
         {
@@ -64,7 +59,7 @@ axsw_if_topology_init(axiom_topology_t *topology)
         }
     }
 
-    for (node_index = 0; node_index < AXIOM_NUM_NODES; node_index++)
+    for (node_index = 0; node_index < num_nodes; node_index++)
     {
         for (if_index = 0; if_index < AXIOM_NUM_INTERFACES; if_index++)
         {
